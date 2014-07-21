eccaf260ffb7bb3d0599d665b80b8a374c201c4f
Matej Kramny <git@matej.me>
2014-07-21 03:38:22 +0100

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 cd /srv/app
+if [ $? -ne 0 ]; then
+  echo '/srv/app does not exist'
+  exit 3
+fi
+
 npm install
+if [ $? -ne 0 ]; then
+  echo npm failed to install
+  exit 2
+fi
 
 npm start 2>&1
+if [ $? -ne 0 ]; then
+  echo App Quit
+  exit 1
+fi
+
+exit 0
